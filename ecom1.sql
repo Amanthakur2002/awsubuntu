c687d07aaa818370b86754b5f2652a8b877be9c9
Amanthakur2002 <125248018+Amanthakur2002@users.noreply.github.com>
2023-04-06 13:45:37 +0530

    Add files via upload

---
+++ ecom1.sql
@@ -0,0 +1,400 @@
+-- MariaDB dump 10.19  Distrib 10.4.27-MariaDB, for Win64 (AMD64)
+--
+-- Host: localhost    Database: e_commerce
+-- ------------------------------------------------------
+-- Server version	10.4.27-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `cart`
+--
+
+DROP TABLE IF EXISTS `cart`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `cart` (
+  `uid` varchar(10) DEFAULT NULL,
+  `product_id` varchar(10) DEFAULT NULL,
+  `quantity` int(11) DEFAULT NULL,
+  `total` int(11) DEFAULT NULL,
+  `payment_id` varchar(20) DEFAULT NULL,
+  KEY `uid` (`uid`),
+  KEY `product_id` (`product_id`),
+  KEY `payment_id` (`payment_id`),
+  CONSTRAINT `cart_ibfk_1` FOREIGN KEY (`uid`) REFERENCES `login` (`uid`),
+  CONSTRAINT `cart_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `products` (`Product_id`),
+  CONSTRAINT `cart_ibfk_3` FOREIGN KEY (`payment_id`) REFERENCES `payment_status` (`payment_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cart`
+--
+
+LOCK TABLES `cart` WRITE;
+/*!40000 ALTER TABLE `cart` DISABLE KEYS */;
+/*!40000 ALTER TABLE `cart` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `login`
+--
+
+DROP TABLE IF EXISTS `login`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `login` (
+  `uid` varchar(10) NOT NULL,
+  `password` varchar(50) DEFAULT NULL,
+  `Register_on` datetime,
+  `status` varchar(8) DEFAULT 'deactive',
+  PRIMARY KEY (`uid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `login`
+--
+
+LOCK TABLES `login` WRITE;
+/*!40000 ALTER TABLE `login` DISABLE KEYS */;
+INSERT INTO `login` VALUES ('1','12345','2023-03-01 14:22:34','deactive'),('2','12345','2023-03-01 17:18:30','deactive'),('4','12345','2023-03-03 12:00:31','deactive'),('5','1234567','2023-03-01 12:52:35','deactive');
+/*!40000 ALTER TABLE `login` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `orders`
+--
+
+DROP TABLE IF EXISTS `orders`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `orders` (
+  `order_id` varchar(20) DEFAULT NULL,
+  `uid` varchar(10) DEFAULT NULL,
+  `order_date` date DEFAULT NULL,
+  `payment` varchar(20) DEFAULT NULL,
+  `shipping_address` varchar(100) DEFAULT NULL,
+  `order_total` int(11) DEFAULT NULL,
+  `order_status` varchar(10) DEFAULT NULL,
+  `product_id` varchar(10) DEFAULT NULL,
+  KEY `product_id` (`product_id`),
+  CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `products` (`Product_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `orders`
+--
+
+LOCK TABLES `orders` WRITE;
+/*!40000 ALTER TABLE `orders` DISABLE KEYS */;
+/*!40000 ALTER TABLE `orders` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `payment_status`
+--
+
+DROP TABLE IF EXISTS `payment_status`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `payment_status` (
+  `payment_id` varchar(20) DEFAULT NULL,
+  `status` varchar(10) DEFAULT NULL,
+  KEY `payment_id` (`payment_id`),
+  CONSTRAINT `payment_status_ibfk_1` FOREIGN KEY (`payment_id`) REFERENCES `user_payment` (`payment_id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `payment_status`
+--
+
+LOCK TABLES `payment_status` WRITE;
+/*!40000 ALTER TABLE `payment_status` DISABLE KEYS */;
+/*!40000 ALTER TABLE `payment_status` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `product_category`
+--
+
+DROP TABLE IF EXISTS `product_category`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `product_category` (
+  `category_id` varchar(10) NOT NULL,
+  `category_name` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`category_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `product_category`
+--
+
+LOCK TABLES `product_category` WRITE;
+/*!40000 ALTER TABLE `product_category` DISABLE KEYS */;
+/*!40000 ALTER TABLE `product_category` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `products`
+--
+
+DROP TABLE IF EXISTS `products`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `products` (
+  `Product_id` varchar(10) NOT NULL,
+  `product_name` varchar(30) DEFAULT NULL,
+  `category_id` varchar(10) DEFAULT NULL,
+  `description` varchar(100) DEFAULT NULL,
+  `product_image` varchar(50) DEFAULT NULL,
+  `shop_id` varchar(10) DEFAULT NULL,
+  `price` int(10) DEFAULT NULL,
+  `stocks` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`Product_id`),
+  KEY `shop_id` (`shop_id`),
+  KEY `category_id` (`category_id`),
+  CONSTRAINT `products_ibfk_1` FOREIGN KEY (`shop_id`) REFERENCES `seller_shop` (`shop_id`),
+  CONSTRAINT `products_ibfk_2` FOREIGN KEY (`category_id`) REFERENCES `product_category` (`category_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `products`
+--
+
+LOCK TABLES `products` WRITE;
+/*!40000 ALTER TABLE `products` DISABLE KEYS */;
+/*!40000 ALTER TABLE `products` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `registration`
+--
+
+DROP TABLE IF EXISTS `registration`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `registration` (
+  `uid` varchar(20) NOT NULL,
+  `username` varchar(20) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `mobile` varchar(10) DEFAULT NULL,
+  `gender` char(1) DEFAULT NULL CHECK (`gender` in ('m','f')),
+  `address_line1` varchar(20) NOT NULL,
+  `address_landmark` varchar(20) DEFAULT NULL,
+  `state` varchar(20) NOT NULL,
+  `city` varchar(20) NOT NULL,
+  `pincode` char(6) NOT NULL,
+  `aadhar` char(12) DEFAULT NULL,
+  `profile_photo` varchar(50) DEFAULT NULL,
+  `aadhar_photo` varchar(50) DEFAULT NULL,
+  `dob` date DEFAULT NULL,
+  `doj` date DEFAULT NULL,
+  PRIMARY KEY (`uid`),
+  UNIQUE KEY `email` (`email`),
+  UNIQUE KEY `mobile` (`mobile`),
+  UNIQUE KEY `aadhar` (`aadhar`),
+  CONSTRAINT `fkadminuid` FOREIGN KEY (`uid`) REFERENCES `login` (`uid`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `registration`
+--
+
+LOCK TABLES `registration` WRITE;
+/*!40000 ALTER TABLE `registration` DISABLE KEYS */;
+INSERT INTO `registration` VALUES ('5','aman','ayush@gmail.com','1134567890','m','jhawaharChowk','bulevardStreet','MP','bhopal','462003','113456789012','photo.jpg','aadhar.jpg','0000-00-00','0000-00-00');
+/*!40000 ALTER TABLE `registration` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `role_assign`
+--
+
+DROP TABLE IF EXISTS `role_assign`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `role_assign` (
+  `uid` varchar(20) NOT NULL,
+  `roleid` varchar(2) NOT NULL,
+  `roleassigon` datetime DEFAULT sysdate(),
+  `roleupdateon` datetime DEFAULT sysdate(),
+  PRIMARY KEY (`uid`,`roleid`),
+  KEY `fkrole` (`roleid`),
+  CONSTRAINT `fkrole` FOREIGN KEY (`roleid`) REFERENCES `roles` (`roleid`) ON DELETE CASCADE,
+  CONSTRAINT `fkroleuid` FOREIGN KEY (`uid`) REFERENCES `registration` (`uid`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `role_assign`
+--
+
+LOCK TABLES `role_assign` WRITE;
+/*!40000 ALTER TABLE `role_assign` DISABLE KEYS */;
+/*!40000 ALTER TABLE `role_assign` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `roles`
+--
+
+DROP TABLE IF EXISTS `roles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `roles` (
+  `roleid` varchar(2) NOT NULL,
+  `role_name` varchar(16) DEFAULT NULL,
+  PRIMARY KEY (`roleid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `roles`
+--
+
+LOCK TABLES `roles` WRITE;
+/*!40000 ALTER TABLE `roles` DISABLE KEYS */;
+INSERT INTO `roles` VALUES ('1','admin'),('2','shopkeeper'),('3','user'),('4','manager'),('5','worker');
+/*!40000 ALTER TABLE `roles` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `seller_account_details`
+--
+
+DROP TABLE IF EXISTS `seller_account_details`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `seller_account_details` (
+  `bank_name` varchar(30) DEFAULT NULL,
+  `account_no` varchar(20) DEFAULT NULL,
+  `branch` varchar(20) DEFAULT NULL,
+  `IFSC_code` varchar(20) DEFAULT NULL,
+  `uid` varchar(10) DEFAULT NULL,
+  KEY `uid` (`uid`),
+  CONSTRAINT `seller_account_details_ibfk_1` FOREIGN KEY (`uid`) REFERENCES `login` (`uid`),
+  CONSTRAINT `seller_account_details_ibfk_2` FOREIGN KEY (`uid`) REFERENCES `login` (`uid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `seller_account_details`
+--
+
+LOCK TABLES `seller_account_details` WRITE;
+/*!40000 ALTER TABLE `seller_account_details` DISABLE KEYS */;
+/*!40000 ALTER TABLE `seller_account_details` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `seller_products`
+--
+
+DROP TABLE IF EXISTS `seller_products`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `seller_products` (
+  `product_id` varchar(10) DEFAULT NULL,
+  `shop_id` varchar(10) DEFAULT NULL,
+  `category_id` varchar(10) DEFAULT NULL,
+  `quantity` int(11) DEFAULT NULL,
+  `price` int(11) DEFAULT NULL,
+  KEY `product_id` (`product_id`),
+  KEY `shop_id` (`shop_id`),
+  CONSTRAINT `seller_products_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `products` (`Product_id`),
+  CONSTRAINT `seller_products_ibfk_2` FOREIGN KEY (`shop_id`) REFERENCES `seller_shop` (`shop_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `seller_products`
+--
+
+LOCK TABLES `seller_products` WRITE;
+/*!40000 ALTER TABLE `seller_products` DISABLE KEYS */;
+/*!40000 ALTER TABLE `seller_products` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `seller_shop`
+--
+
+DROP TABLE IF EXISTS `seller_shop`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `seller_shop` (
+  `shop_id` varchar(10) NOT NULL,
+  `shop_address` varchar(100) DEFAULT NULL,
+  `GST_no` varchar(20) DEFAULT NULL,
+  `owner_name` varchar(20) DEFAULT NULL,
+  `uid` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`shop_id`),
+  KEY `uid` (`uid`),
+  CONSTRAINT `seller_shop_ibfk_1` FOREIGN KEY (`uid`) REFERENCES `login` (`uid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `seller_shop`
+--
+
+LOCK TABLES `seller_shop` WRITE;
+/*!40000 ALTER TABLE `seller_shop` DISABLE KEYS */;
+/*!40000 ALTER TABLE `seller_shop` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_payment`
+--
+
+DROP TABLE IF EXISTS `user_payment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user_payment` (
+  `payment_id` varchar(20) NOT NULL,
+  `uid` varchar(10) DEFAULT NULL,
+  `account_no` varchar(20) DEFAULT NULL,
+  `payment_type` varchar(20) DEFAULT NULL,
+  `payment_date` date DEFAULT NULL,
+  PRIMARY KEY (`payment_id`),
+  KEY `uid` (`uid`),
+  CONSTRAINT `user_payment_ibfk_1` FOREIGN KEY (`uid`) REFERENCES `login` (`uid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_payment`
+--
+
+LOCK TABLES `user_payment` WRITE;
+/*!40000 ALTER TABLE `user_payment` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user_payment` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-04-06 11:42:00
